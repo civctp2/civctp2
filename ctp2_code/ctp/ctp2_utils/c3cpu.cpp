9a9859132f00740316a55acfb3af715aa07bb389
Fromafar <Fromafar@34d2fc0c-33df-0310-8a34-85f8040ba873>
2003-12-15 23:11:31 +0000

    - Possible crash fix: do not use cpuinf32.dll to examine the CPU.
    git-svn-id: http://ctp2.darkdust.net/anonsvn/trunk@49 34d2fc0c-33df-0310-8a34-85f8040ba873

--- ctp2_code/ctp/ctp2_utils/c3cpu.cpp
+++ ctp2_code/ctp/ctp2_utils/c3cpu.cpp
@@ -1,16 +1,39 @@
-
-
-
-
-
-
-
-
-
+//----------------------------------------------------------------------------
+//
+// Project      : Call To Power 2
+// File type    : C++ source
+// Description  : Retrieve CPU information through cpuinf32.dll.
+//
+//----------------------------------------------------------------------------
+//
+// Disclaimer
+//
+// THIS FILE IS NOT GENERATED OR SUPPORTED BY ACTIVISION.
+//
+// This material has been developed at apolyton.net by the Apolyton CtP2 
+// Source Code Project. Contact the authors at ctp2source@apolyton.net.
+//
+//----------------------------------------------------------------------------
+//
+// Compiler flags
+// 
+// ACTIVISION_ORIGINAL		
+// - When defined, generates the original Activision code.
+// - When not defined, generates the modified Apolyton code.
+//
+//----------------------------------------------------------------------------
+//
+// Modifications from the original Activision code:
+//
+// - Stubbed out all functionality to prevent crashes on some machines.
+//
+//----------------------------------------------------------------------------
 
 
 #include "c3.h"
 #include "c3cpu.h"
+
+#if defined(ACTIVISION_ORIGINAL)
 
 HINSTANCE hLibrary;				
 
@@ -158,4 +181,23 @@ MBCHAR *c3cpu_GetString(void)
 	return g_cpuString;
 }
 
+#else
 
+// Stub versions of the above
+
+MBCHAR g_cpuString[] = "";
+
+void c3cpu_Initialize(void)
+{
+}
+
+void c3cpu_Examine(void)
+{
+}
+
+MBCHAR * c3cpu_GetString(void)
+{
+	return g_cpuString;
+}
+
+#endif
