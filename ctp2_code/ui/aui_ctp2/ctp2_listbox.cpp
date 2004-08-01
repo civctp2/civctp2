78c51e4a21142c5b16bddc56427e76dbf5c4c9f6
Martin Gühmann <Martin Ghmann@34d2fc0c-33df-0310-8a34-85f8040ba873>
2004-08-01 20:08:48 +0000

    2004.06.14 ..\ctp2_data\XXXXXX\gamedata\ldl_str.txt ..\ctp2_data\XXXXXX\uidata\layouts\km_screen.ldl
    Where XXXXXX stands for english, french, german, italien, japanese and spanish
    ldl_str.txt file.
    
    Translations to command editions by Lui2:
    
    
    2004.06.19
    ..\ctp2_data\XXXXXX\gamedata\ldl_str.txt
    ..\ctp2_data\XXXXXX\gamedata\tips_str.txt
    ..\ctp2_data\XXXXXX\uidata\layouts/citystatus.ldl
    
    Where XXXXXX stands for the german and english directories. I will post the
    strings that must be translated, in the specific TranslationCenter Thread.
    
    
    2004.07.02
    ..\ctp2_data\english\gamedata\ldl_str.txt
    ..\ctp2_data\german\gamedata\ldl_str.txt
    
    Added user interface option by Fromafar: Offer a choice between showing the
    intersection (default/original game) or the union of the possible orders of
    the units in an army, to make e.g. bombarding easier.
    
    You may toggle this choice in-game through the Advanced options menu (Show All
    Army Orders), or edit the ShowOrderUnion line in your userprofile.txt.
    
    04.07.2004
    ..\ctp2_data\default\gamedata\unit.txt
    ..\ctp2_data\default\gamedata\script.slc
    
    ..\ctp2_data\german\gamedata\info_str.txt
    ..\ctp2_data\english\gamedata\info_str.txt
    
    Modifications by Lui2:
    Added two new messages to distinct message text after investigating a city
    depending if the unit was caught or not. I didn't like that the message was
    telling me that my spy was caught, but he wasn't.
    Two strings are added to the info_str.txt and a call added to script.slc
    files.
    INVESTIGATE_CITY_FAILED_ESC_VICTIM
    INVESTIGATE_CITY_FAILED_ESC_AGRESSOR
    
    Also changed the EliteChance in Units.txt to an absolute value, like it's used
    in the program.
    Look here for exact background.
    
    Also did one additional minor fix to the units.txt by changing the size of the
    UNIT_TANK from small to medium.
    
    I add information about the new strings to the TranslationCenter thread.
    
    
    2004.07.08
    ctp2_data\default\gamedata\Const.txt
    
    Activision patch reimplementation by kaan:
    Bug #12 "Prevented cities from revolting twice in a row".
    
    The amount of turns minimum between revolts was exposed to const.txt.
    
    
    2004.07.25
    ..\ctp2_data\english\gamedata\GreatLibary.txt
    ..\ctp2_data\german\gamedata\GreatLibary.txt
    ..\ctp2_data\english\gamedata\ldl_str.txt
    ..\ctp2_data\french\gamedata\ldl_str.txt
    ..\ctp2_data\german\gamedata\ldl_str.txt
    ..\ctp2_data\italian\gamedata\ldl_str.txt
    ..\ctp2_data\japanese\gamedata\ldl_str.txt
    ..\ctp2_data\spanish\gamedata\ldl_str.txt
    ..\ctp2_data\french\gamedata\tips_str.txt
    ..\ctp2_data\italian\gamedata\tips_str.txt
    ..\ctp2_data\spanish\gamedata\tips_str.txt
    ..\ctp2_data\spanish\gamedata\info_str.txt
    ..\ctp2_data\default\gamedate\const.txt
    
    GreatLibary.txt: Chnaged entries from
    <L:DATABASE_BUILDINGS,IMPROVE_CORNUCOPIC_VATS>Cornucopic Vats<e>
    to
    <L:DATABASE_BUILDINGS,IMPROVE_CORNUCOPIC_VAT>Cornucopic Vats<e>
    
    Other language text files: Added missing strings at least in English that the
    creators of the new strings forgot to add to these files.
    
    In addition to ldl_str.txt updated the version strings to match with the
    latest playtest version.
    
    Const.txt: Moved the new flag MIN_TURNS_BETWEEN_REVOLT to the end of this file
    as the flag at its original position made the game hang.
    
    
    git-svn-id: http://ctp2.darkdust.net/anonsvn/trunk@193 34d2fc0c-33df-0310-8a34-85f8040ba873

--- ctp2_code/ui/aui_ctp2/ctp2_listbox.cpp
+++ ctp2_code/ui/aui_ctp2/ctp2_listbox.cpp
@@ -26,8 +26,6 @@
 // Modifications from the original Activision code:
 //
 // - Prevented crash (access to deleted memory).
-// - #01 Optimized sorting of list entries.
-//   (L. Hirth 7/2004)
 //
 //----------------------------------------------------------------------------
 
@@ -389,19 +387,13 @@ AUI_ERRCODE ctp2_ListBox::SortByColumn(
 	
 	if (m_numRows <= 1) return AUI_ERRCODE_OK;
 
-#ifndef ACTIVISION_ORIGINAL // #01 Optimized sorting of list entries 
-	sint32 cycles = 0;	
-#endif
+	
 	BOOL changed;
 	do
 	{
 		changed = FALSE;
 
-#ifdef ACTIVISION_ORIGINAL // #01 Optimized sorting of list entries 
 		for ( sint32 i = 0; i < m_numRows-1; i++ )
-#else
-		for ( sint32 i = 0; i < m_numRows-1-cycles; i++ )
-#endif
 		{
 			ListPos positionA = m_pane->ChildList()->FindIndex( i );
 			ListPos positionB = positionA;
@@ -417,9 +409,6 @@ AUI_ERRCODE ctp2_ListBox::SortByColumn(
 				changed = TRUE;
 			}
 		}
-#ifndef ACTIVISION_ORIGINAL // #01 Optimized sorting of list entries 
-	++cycles;	
-#endif
 	} while ( changed );
 
 	RepositionItems();
