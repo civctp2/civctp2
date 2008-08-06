15d9da43acd726bc2a6748ec0f6c2c628c602e79
Fromafar <Fromafar@34d2fc0c-33df-0310-8a34-85f8040ba873>
2008-08-06 18:46:08 +0000

    Added map generator module definition file for Visual Studio 2005/2008. Corrected crashes/memory leaks (for multiplayer).
    git-svn-id: http://ctp2.darkdust.net/anonsvn/trunk@882 34d2fc0c-33df-0310-8a34-85f8040ba873

--- ctp2_code/net/general/net_cell.cpp
+++ ctp2_code/net/general/net_cell.cpp
@@ -1,28 +1,58 @@
-#include "c3.h"
-#include "Cell.h"
+//----------------------------------------------------------------------------
+//
+// Project      : Call To Power 2
+// File type    : C++ source
+// Id           : $Id$
+//
+//----------------------------------------------------------------------------
+//
+// Disclaimer
+//
+// THIS FILE IS NOT GENERATED OR SUPPORTED BY ACTIVISION.
+//
+// This material has been developed at apolyton.net by the Apolyton CtP2 
+// Source Code Project. Contact the authors at ctp2source@apolyton.net.
+//
+//----------------------------------------------------------------------------
+//
+// Compiler flags
+//
+// USE_WORLDSIZE_CLASS
+// - Implementation with separate WorldSize class (decoupled from MapPoint)
+//
+//----------------------------------------------------------------------------
+//
+// Modifications from the original Activision code:
+//
+// - Corrected possible crashes and memory leaks
+//
+//----------------------------------------------------------------------------
+//
+/// \file   net_cell.cpp
+/// \brief  Multiplayer cell packet handling (definitions)
 
-#include "Globals.h"        // k_GAME_OBJ_TERRAIN_IMPROVEMENT
+#include "c3.h"
 #include "net_cell.h"
+
+#include "Cell.h"
+#include "cellunitlist.h"
+#include "Globals.h"        // k_GAME_OBJ_TERRAIN_IMPROVEMENT
+#include "GoodActor.h"
+#include "GoodyHuts.h"
 #include "net_util.h"
 #include "network.h"
-#include "TerrImprove.h"
-#include "GoodyHuts.h"
-#include "TileInfo.h"
-#include "GoodActor.h"
-#include "tiledmap.h"
-#include "cellunitlist.h"
-#include "TradeDynArr.h"
 #include "ObjPool.h"
+#include "TerrImprove.h"
 #include "TerrImprovePool.h"
+#include "tiledmap.h"
+#include "TileInfo.h"
+#include "TradeDynArr.h"
 #include "World.h"
 
 #define SEND_MOVE_COST
 
-
-void
-NetCellData::Packetize(uint8* buf, uint16& size)
+void NetCellData::Packetize(uint8 * buf, uint16 & size)
 {
-	size = 0;
 	buf[0] = 'G';
 	buf[1] = 'C';
 	size = 2;
@@ -36,21 +66,19 @@ NetCellData::Packetize(uint8* buf, uint16& size)
 	PUSHBYTE(m_cell->m_cellOwner);
 	PUSHLONG(m_cell->m_city.m_id);
 
-	sint32 i;
-	PUSHBYTE((uint8)m_cell->m_objects->Num());
-	for(i = 0; i < m_cell->m_objects->Num(); i++) {
-		PUSHLONG(m_cell->m_objects->Access(i).m_id);
+	int const objectCount = (m_cell->m_objects) ? m_cell->m_objects->Num() : 0;
+	PUSHBYTE(objectCount);
+	for (int i = 0; i < objectCount; ++i)
+	{
+		PUSHLONG(m_cell->m_objects->Get(i).m_id);
 	}
 }
 
 
-void
-NetCellData::Unpacketize(uint16 id, uint8* buf, uint16 size)
+void NetCellData::Unpacketize(uint16 id, uint8* buf, uint16 size)
 {
-	uint16 pos = 0;
-
 	Assert(buf[0] == 'G' && buf[1] == 'C');
-	pos = 2;
+	uint16 pos = 2;
 
 	PULLSHORT(m_x);
 	PULLSHORT(m_y);
@@ -58,7 +86,7 @@ NetCellData::Unpacketize(uint16 id, uint8* buf, uint16 size)
 	MapPoint mp(m_x, m_y);
 
 	DPRINTF(k_DBG_NET, ("Handling cell at %d/%d\n", m_x, m_y));
-	m_cell = g_theWorld->GetCell(mp);
+	m_cell = g_theWorld->AccessCell(mp);
 	PULLLONG(m_cell->m_env);
 	PULLBYTE(m_cell->m_terrain_type);
 	sint16 old_move_cost = m_cell->m_move_cost;
@@ -68,44 +96,40 @@ NetCellData::Unpacketize(uint16 id, uint8* buf, uint16 size)
 		g_theWorld->SetCapitolDistanceDirtyFlags(0xffffffff);
 	PULLSHORT(m_cell->m_continent_number);
 	
-	
 	uint8 owner;
 	PULLBYTE(owner);
 	m_cell->SetOwner(owner);
 	PULLLONG(m_cell->m_city.m_id);
 
-	delete m_cell->m_objects;
-	m_cell->m_objects = NULL;
-
 	uint8 numObjects;
 	PULLBYTE(numObjects);
-	if(numObjects > 0) {
-		m_cell->m_objects = new DynamicArray<ID>;
-		uint8 i;
-		for(i = 0; i < numObjects; i++) {
-			uint32 id;
-			PULLLONG(id);
-			if((id & k_ID_TYPE_MASK) == k_BIT_GAME_OBJ_TYPE_TERRAIN_IMPROVEMENT) {
-				if(!g_theTerrainImprovementPool->IsValid(id)) {
-					continue;
-				}
-			}
+	delete m_cell->m_objects;
+	m_cell->m_objects = (numObjects > 0) ? new DynamicArray<ID> : NULL;
 
+	for (uint8 i = 0; i < numObjects; ++i)
+	{
+		uint32 id;
+		PULLLONG(id);
+		if (    ((id & k_ID_TYPE_MASK) == k_BIT_GAME_OBJ_TYPE_TERRAIN_IMPROVEMENT)
+		     && g_theTerrainImprovementPool->IsValid(id)
+		   )
+		{
 			m_cell->m_objects->Insert(id);
 		}
 	}
 
-	if(g_tiledMap) {
+	if (g_tiledMap)
+	{
 		g_tiledMap->PostProcessTile(mp, g_theWorld->GetTileInfo(mp));
 		g_tiledMap->TileChanged(mp);
 
 		MapPoint npos;
-		for(WORLD_DIRECTION d = NORTH; d < NOWHERE; 
-			d = (WORLD_DIRECTION)((sint32)d + 1)) {
-			if(mp.GetNeighborPosition(d, npos)) {
-				g_tiledMap->PostProcessTile(
-					npos, 
-					g_theWorld->GetTileInfo(npos));
+		for (int d = 0; d < NOWHERE; ++d)
+		{
+			if (mp.GetNeighborPosition(static_cast<WORLD_DIRECTION>(d), npos))
+			{
+				g_tiledMap->PostProcessTile
+					(npos, g_theWorld->GetTileInfo(npos));
 				g_tiledMap->TileChanged(npos);
 				g_tiledMap->RedrawTile(&npos);
 			}
@@ -118,63 +142,63 @@ NetCellData::Unpacketize(uint16 id, uint8* buf, uint16 size)
 
 void NetCellList::Packetize(uint8* buf, uint16& size)
 {
-	MapPoint* mapsize = g_theWorld->GetSize();
-
-	uint8 cells = 0;
 	size = 0;
 
 	PUSHID(k_PACKET_CELL_LIST_ID);
-
+	
 	MapPoint pos(m_x, m_y);
 	PUSHLONG(g_network.PackedPos(pos));
 	PUSHBYTE(m_cells);
 
-	int sx = m_x;
-	int sy = m_y;
+	size_t cells = 0;
 
-	for(sint32 x = sx; x < mapsize->x; x++)
+#if defined(USE_WORLDSIZE_CLASS)
+	WorldSize mapsize = g_theWorld->GetSize();
+#else
+	MapPoint  mapsize = *g_theWorld->GetSize();
+#endif
+	for (sint32 x = m_x; x < mapsize.x; x++)
 	{
-		sx = 0;
-		for(sint32 y = sy; y < mapsize->y; y++)
+		for (sint32 y = m_y; y < mapsize.y; y++)
 		{
-			sy = 0;
 			MapPoint mp(x,y);
-			Cell* cell = g_theWorld->GetCell(mp);
+			Cell const * cell = g_theWorld->GetCell(mp);
 			PUSHLONG(cell->m_env);
-
+			
 			uint8 terrainPlusFlags = cell->m_terrain_type;
-			if(cell->GetCity().m_id != 0)
+			if (cell->GetCity().m_id != 0)
 			{
 				terrainPlusFlags |= 0x80;
 			}
-
-			if(cell->m_jabba)
+			if (cell->m_jabba)
 			{
 				terrainPlusFlags |= 0x40;
 			}
 			PUSHBYTE(terrainPlusFlags);
-			PUSHBYTE(cell->m_cellOwner);
-			
-			PUSHLONG(cell->m_city.m_id);
 
-			if(cell->m_jabba)
+			PUSHBYTE(cell->m_cellOwner);
+			PUSHLONG(cell->m_city.m_id);
+			
+			if (cell->m_jabba)
 			{
 				PUSHBYTE((uint8)cell->m_jabba->m_typeValue);
-				PUSHSHORT((uint16)cell->m_jabba->m_value)
 				Assert(cell->m_jabba->m_value >= 0 && cell->m_jabba->m_value < k_VALUE_RANGE);
+				PUSHSHORT((uint16)cell->m_jabba->m_value)
 			}
-
+			
 #ifdef SEND_MOVE_COST
 			PUSHSHORT(cell->m_move_cost);
 #endif
-			PUSHBYTE((uint8)cell->m_objects->Num());
-			for(sint32 i = 0; i < cell->m_objects->Num(); i++)
+
+			int const objectCount = (cell->m_objects) ? cell->m_objects->Num() : 0;
+			PUSHBYTE(objectCount);
+			for (int i = 0; i < objectCount; ++i)
 			{
-				PUSHLONG(cell->m_objects->Access(i).m_id);
+				PUSHLONG(cell->m_objects->Get(i).m_id);
 			}
 
-			cells++;
-			if(cells >= m_cells)
+			++cells;
+			if (cells >= m_cells)
 			{
 				return;
 			}
@@ -184,9 +208,6 @@ void NetCellList::Packetize(uint8* buf, uint16& size)
 
 void NetCellList::Unpacketize(uint16 id, uint8* buf, uint16 len)
 {
-	MapPoint* mapsize = g_theWorld->GetSize();
-	uint16 cells = 0;
-
 	Assert(buf[0] = 'G' && buf[1] == 'L');
 
 	uint16 pos = 2;
@@ -198,48 +219,33 @@ void NetCellList::Unpacketize(uint16 id, uint8* buf, uint16 len)
 	m_y = mapPos.y;
 	PULLBYTE(m_cells);
 
-	int sx = m_x;
-	int sy = m_y;
-	
-	for(sint32 x = sx; x < mapsize->x; x++) {
-		
-		
-		sx = 0;
-		for(sint32 y = sy; y < mapsize->y; y++) {
-			sy = 0;
+	size_t cells = 0;
+
+#if defined(USE_WORLDSIZE_CLASS)
+	WorldSize mapsize = g_theWorld->GetSize();
+#else
+	MapPoint  mapsize = *g_theWorld->GetSize();
+#endif
+	for (sint32 x = m_x; x < mapsize.x; x++)
+	{
+		for (sint32 y = m_y; y < mapsize.y; y++)
+		{
 			MapPoint mp(x,y);
-			Cell* cell = g_theWorld->GetCell(mp);
+			Cell* cell = g_theWorld->AccessCell(mp);
 			
-			if(cell->m_unit_army) {
-				delete cell->m_unit_army;
-				cell->m_unit_army = NULL;
-			}
+			delete cell->m_unit_army;
+			cell->m_unit_army = NULL;
+
 			cell->SetCity(Unit());
 			
-			if(cell->m_objects) {
-				delete cell->m_objects;
-				cell->m_objects = NULL;
-			}
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+			delete cell->m_objects;
+			cell->m_objects = NULL;
 			
 			PULLLONG(cell->m_env);
 			
 			uint8 terrainPlusFlags;
 			PULLBYTE(terrainPlusFlags);
-			
-			
 			cell->m_terrain_type = terrainPlusFlags & 0x3f;
-			
 			
 			uint8 owner;
 			PULLBYTE(owner);
@@ -247,27 +253,21 @@ void NetCellList::Unpacketize(uint16 id, uint8* buf, uint16 len)
 
 			PULLLONG(cell->m_city.m_id);
 			
-			
-			if(cell->m_jabba) {
-				delete cell->m_jabba;
-				cell->m_jabba = NULL;
-			}
-			uint8 tmp;
-			uint16 value;
-			if(terrainPlusFlags & 0x40) {
+			delete cell->m_jabba;
+			if (terrainPlusFlags & 0x40)
+			{
+				uint8 tmp;
 				PULLBYTE(tmp);
+				uint16 value;
 				PULLSHORT(value);
-				cell->m_jabba = new GoodyHut(tmp & 0x7f,
-											 (uint32)value);
+				cell->m_jabba = new GoodyHut(tmp & 0x7f, (uint32) value);
 				Assert(cell->m_jabba->m_value >= 0 && cell->m_jabba->m_value < k_VALUE_RANGE);
 			}
+			else
+			{
+				cell->m_jabba = NULL;
+			}
 				
-			
-			
-			
-
-			
-
 #ifdef SEND_MOVE_COST
 			uint16 actualCost;
 			PULLSHORT(actualCost);
@@ -275,42 +275,32 @@ void NetCellList::Unpacketize(uint16 id, uint8* buf, uint16 len)
 
 			uint8 numObjects;
 			PULLBYTE(numObjects);
-			if(numObjects > 0) {
-				cell->m_objects = new DynamicArray<ID>;
-				uint8 i;
-				for(i = 0; i < numObjects; i++) {
-					uint32 id;
-					PULLLONG(id);
-					cell->m_objects->Insert(id);
-				}
+			delete cell->m_objects;
+			cell->m_objects = (numObjects > 0) ? new DynamicArray<ID> : NULL;
+
+			for (uint8 i = 0; i < numObjects; i++)
+			{
+				uint32 id;
+				PULLLONG(id);
+				cell->m_objects->Insert(id);
 			}
+
 			cell->CalcTerrainMoveCost();
-#ifdef SEND_MOVE_COST
-			
-			
-			
-#endif
 
-			
-			
-
-			if(g_theWorld->GetTileInfo(mp) && g_theWorld->GetTileInfo(mp)->m_goodActor) {
+			if (g_theWorld->GetTileInfo(mp))
+			{
 				delete g_theWorld->GetTileInfo(mp)->m_goodActor;
 				g_theWorld->GetTileInfo(mp)->m_goodActor = NULL;
 			}
+
 			cells++;
-			if(cells >= m_cells) {
+			if (cells >= m_cells)
+			{
 				Assert(pos == len);
 				return;
 			}
 		}
 	}
-}
-
-NetCellUnitOrder::NetCellUnitOrder(sint32 x, sint32 y)
-{
-	m_x = (uint16)x;
-	m_y = (uint16)y;
 }
 
 void NetCellUnitOrder::Packetize(uint8 *buf, uint16 &size)
@@ -321,18 +311,15 @@ void NetCellUnitOrder::Packetize(uint8 *buf, uint16 &size)
 	PUSHSHORT(m_y);
 	CellUnitList *units = g_theWorld->GetCell(m_x, m_y)->UnitArmy();
 
-	Assert(units);
-	if(!units)
-		return;
-
-	Assert(units->Num() > 1);
-	if(units->Num() <= 1)
-		return;
-
-	uint8 i, n = (uint8)units->Num();
-	PUSHBYTE(n);
-	for(i = 0; i < n; i++) {
-		PUSHLONG((uint32)units->Access(i));
+	Assert(units && (units->Num() > 1));
+	if (units && (units->Num() > 1))
+	{
+		uint8 n = (uint8)units->Num();
+		PUSHBYTE(n);
+		for (uint8 i = 0; i < n; i++)
+		{
+			PUSHLONG((uint32)units->Access(i));
+		}
 	}
 }
 
@@ -346,20 +333,17 @@ void NetCellUnitOrder::Unpacketize(uint16 id, uint8 *buf, uint16 size)
 	PULLSHORT(m_x);
 	PULLSHORT(m_y);
 	CellUnitList *units = g_theWorld->GetCell(m_x, m_y)->UnitArmy();
-	Assert(units);
-	if(!units)
-		return;
-
-	Assert(units->Num() > 1);
-	if(units->Num() <= 1)
-		return;
-
-	uint8 i, n;
-	PULLBYTE(n);
-	Assert(n == (uint8)units->Num());
-	for(i = 0; i < n; i++) {
-		Unit u;
-		PULLLONGTYPE(u, Unit);
-		(*units)[i] = u;
+	Assert(units && (units->Num() > 1));
+	if (units && (units->Num() > 1))
+	{
+		uint8 n;
+		PULLBYTE(n);
+		Assert(n == (uint8)units->Num());
+		for (uint8 i = 0; i < n; i++)
+		{
+			Unit u;
+			PULLLONGTYPE(u, Unit);
+			(*units)[i] = u;
+		}
 	}
 }
--- ctp2_code/net/general/net_cell.h
+++ ctp2_code/net/general/net_cell.h
@@ -1,57 +1,148 @@
+//----------------------------------------------------------------------------
+//
+// Project      : Call To Power 2
+// File type    : C++ header
+// Id           : $Id$
+//
+//----------------------------------------------------------------------------
+//
+// Disclaimer
+//
+// THIS FILE IS NOT GENERATED OR SUPPORTED BY ACTIVISION.
+//
+// This material has been developed at apolyton.net by the Apolyton CtP2 
+// Source Code Project. Contact the authors at ctp2source@apolyton.net.
+//
+//----------------------------------------------------------------------------
+//
+// Compiler flags
+//
+//
+//----------------------------------------------------------------------------
+//
+// Modifications from the original Activision code:
+//
+// - Improved structure.
+//
+//----------------------------------------------------------------------------
+//
+/// \file   net_cell.h
+/// \brief  Multiplayer cell packet handling (declarations)
+
 #ifdef HAVE_PRAGMA_ONCE
 #pragma once
 #endif
-#ifndef _NET_CELL_H_
-#define _NET_CELL_H_
 
-#include "net_packet.h"
+#ifndef NET_CELL_H_
+#define NET_CELL_H_
+
+//----------------------------------------------------------------------------
+// Library dependencies
+//----------------------------------------------------------------------------
+
+// None
+
+//----------------------------------------------------------------------------
+// Export overview
+//----------------------------------------------------------------------------
+
+class NetCellData;
+class NetCellList;
+class NetCellUnitOrder;
+
+//----------------------------------------------------------------------------
+// Project dependencies
+//----------------------------------------------------------------------------
+
+#include "net_packet.h" // Packetizer
+class Cell;
+
+//----------------------------------------------------------------------------
+// Class declarations
+//----------------------------------------------------------------------------
 
 class NetCellData : public Packetizer
 {
 public:
-	NetCellData(Cell* cell, sint32 x, sint32 y) {
-		m_cell = cell;
-		m_x = (uint16)x;
-		m_y = (uint16)y;
-	}
-	NetCellData() {};
+	NetCellData(Cell * cell, sint32 x, sint32 y)
+	:
+		Packetizer	(),
+		m_cell		(cell),
+		m_x			(static_cast<uint16>(x)),
+		m_y			(static_cast<uint16>(y))
+	{};
+
+	NetCellData() 
+	:
+		Packetizer	(),
+		m_cell		(NULL),
+		m_x			(0),
+		m_y			(0)
+	{};
 
 	void Packetize(uint8* buf, uint16& size);
 	void Unpacketize(uint16 id, uint8* buf, uint16 size);
-	Cell* m_cell;
-	uint16 m_x, m_y;
+
+private:
+	Cell * m_cell;
+	uint16 m_x;
+	uint16 m_y;
 };
 
 class NetCellList : public Packetizer
 {
 public:
 	NetCellList(sint32 x, sint32 y) 
-	{
-		m_x = (uint16)x;
-		m_y = (uint16)y;
-		m_cells = 0;
-	}
+	:
+		Packetizer	(),
+		m_x			(static_cast<uint16>(x)),
+		m_y			(static_cast<uint16>(y)),
+		m_cells		(0)
+	{};
 
-	NetCellList() : m_cells(0) {};
+	NetCellList() 
+	:
+		Packetizer	(),
+		m_x			(0),
+		m_y			(0),
+		m_cells		(0)
+	{};
 
 	void Packetize(uint8* buf, uint16& size);
 	void Unpacketize(uint16 id, uint8* buf, uint16 size);
-	uint16 m_x, m_y;
-	uint8 m_cells;
+
+private:
+	uint16 m_x;
+	uint16 m_y;
+	uint8  m_cells;
+
+	friend class Network;
 };
 
 
 class NetCellUnitOrder : public Packetizer
 {
 public:
-	NetCellUnitOrder(sint32 x, sint32 y);
-	NetCellUnitOrder() {}
+	NetCellUnitOrder(sint32 x, sint32 y)
+	:
+		Packetizer	(),
+		m_x			(static_cast<uint16>(x)),
+		m_y			(static_cast<uint16>(y))
+	{};
+
+	NetCellUnitOrder()
+	:
+		Packetizer	(),
+		m_x			(0),
+		m_y			(0)
+	{};
 
 	void Packetize(uint8 *buf, uint16 &size);
 	void Unpacketize(uint16 id, uint8 *buf, uint16 size);
-	uint16 m_x, m_y;
+
+private:
+	uint16 m_x;
+	uint16 m_y;
 };
 
-#else
-class NetCellData;
 #endif
