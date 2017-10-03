5e5f52b517dc8a5baf1520d92a72b140cf9ac9ef
ptitSeb <sebastien.chev@gmail.com>
2017-10-03 23:47:45 +0200

    In case Multithread X11 is needed

--- ctp2_code/ctp/civ3_main.cpp
+++ ctp2_code/ctp/civ3_main.cpp
@@ -1616,6 +1616,7 @@ int WINAPI CivMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
 #endif
 
 #ifdef __GNUC__
+	XInitThreads();
 	ParseCommandLine(iCmdShow, pSzCmdLine);
 #else
 	ParseCommandLine(szCmdLine);
