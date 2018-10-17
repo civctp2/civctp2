3ec4dca6c156dc109eb4a40a6fa2424ca06d0f48
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2018-10-17 23:35:33 +0200

    export userprofile.txt and save/ from .civctp2/

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ XAUTH=/tmp/.docker.xauth
 touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
+mkdir -p $HOME/.civctp2/save/
+touch $HOME/.civctp2/userprofile.txt # file must exist for docker file-vol
 docker run -it \
        --volume=$XSOCK:$XSOCK:rw \
        --volume=$XAUTH:$XAUTH:rw \
@@ -14,6 +16,8 @@ docker run -it \
        --env="XAUTHORITY=${XAUTH}" \
        --env="DISPLAY" \
        --user="diUser" \
+       -v $HOME/.civctp2/userprofile.txt:/opt/ctp2/ctp2_program/ctp/userprofile.txt \
+       -v $HOME/.civctp2/save/:/opt/ctp2/ctp2_program/ctp/save \
        registry.gitlab.com/lynxabraxas/ctp2df/master:builder-latest $@
 
 rm $XAUTH # remove to avoid accumulation of xauth settings
