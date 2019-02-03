7a0db1bfe7ecfb009773ff98fedc3303685fa276
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2019-02-03 10:54:25 +0100

    run.sh example referring to registry.gitlab.com/civctp2/civctp2/master:latest

--- run.sh
+++ run.sh
@@ -9,14 +9,14 @@ mkdir -p $HOME/.civctp2/save/
 touch $HOME/.civctp2/userprofile.txt # file must exist for docker file-vol
 touch $HOME/.civctp2/userkeymap.txt # file must exist for docker file-vol
 ## use -v to specify the folder with OGGs (TrackXX.ogg) for the game music, e.g.:
-## ./run.sh -v $HOME/ctp2CD/ctp2_program/ctp/music/:/opt/ctp2/ctp2_program/ctp/music/:ro registry.gitlab.com/civctp2/ctp2df/test-gui:test ./ctp2
+## ./run.sh -v $HOME/ctp2CD/ctp2_program/ctp/music/:/opt/ctp2/ctp2_program/ctp/music/:ro registry.gitlab.com/civctp2/civctp2/master:latest ./ctp2 fullscreen
 docker run \
        --rm \
        --volume=$XSOCK:$XSOCK:rw \
        --volume=$XAUTH:$XAUTH:rw \
        --device=/dev/dri:/dev/dri \
        --device=/dev/snd:/dev/snd \
-       --env="ALSA_CARD=1" \
+       --env="ALSA_CARD=0" \
        --env="XAUTHORITY=${XAUTH}" \
        --env="DISPLAY" \
        --user="diUser" \
