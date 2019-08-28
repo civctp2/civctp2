7924413202d435995ebb2fe4d66b6e443b7febc2
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2019-08-28 20:51:23 +0200

    left-right layout

--- tools/json2dot.sh
+++ tools/json2dot.sh
@@ -4,5 +4,6 @@ jq -r 'to_entries[] | .key as $k | .value[]? | if $k != . then "\(.) -> \($k)" e
     | cat <(sed -n '/^ICON_ADVANCE_/s|^.*ICON_ADVANCE_\([^[:space:]]*\).*FirstFrame[^"]*"[CM2_]*\([^"]*\).TGA".*$|  ADVANCE_\1 [label=<<TABLE border="0" cellspacing="0" cellborder="0"><TR><TD><IMG SRC="\L\2\E.png"/></TD></TR><TR><TD>\1</TD></TR></TABLE>>, shape=box] |p' $1) - \
     | cat <(echo 'digraph G {') \
           <(echo 'imagepath="pic555"') \
+          <(echo 'rankdir=LR') \
 	  - \
 	  <(echo '}')
