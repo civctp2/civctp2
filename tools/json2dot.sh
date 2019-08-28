c8a33a80c747547ef19c8a20e532ab461c542c26
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2019-08-28 14:47:44 +0200

    remove circular dependencies

--- tools/json2dot.sh
+++ tools/json2dot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-jq -r 'to_entries[] | .key as $k | .value[]? | "\(.) -> \($k)"' \
+jq -r 'to_entries[] | .key as $k | .value[]? | if $k != . then "\(.) -> \($k)" else "" end' \
     | cat <(echo 'digraph G {') - <(echo '}')
