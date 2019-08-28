be77fa51d1fcb5afee98059df9e686d74021c62a
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2019-08-28 15:36:47 +0200

    TGA as node image (apparently) not supported:
    but as output:
    https://www.graphviz.org/doc/info/output.html#d:image_fmts

--- tools/json2dot.sh
+++ tools/json2dot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 jq -r 'to_entries[] | .key as $k | .value[]? | if $k != . then "\(.) -> \($k)" else "" end' \
-    | cat <(sed -n '/^ICON_ADVANCE_/s|^.*ICON_\(ADVANCE_[^[:space:]]*\).*FirstFrame[^"]*"CM2_\([^"]*\)".*$|\1 [image="\L\2"]|p' $1) - \
+    | cat <(sed -n '/^ICON_ADVANCE_/s|^.*ICON_\(ADVANCE_[^[:space:]]*\).*FirstFrame[^"]*"CM2_\([^"]*\).TGA".*$|\1 [image="\L\2.png"]|p' $1) - \
     | cat <(echo 'digraph G {') \
           <(echo 'imagepath="pic555"') \
 	  - \
