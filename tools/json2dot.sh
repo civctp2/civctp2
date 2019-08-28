3acfd91ef40cff6f530cd3441c629ed3ec5a21a2
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2019-08-28 15:26:58 +0200

    icons for nodes

--- tools/json2dot.sh
+++ tools/json2dot.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 jq -r 'to_entries[] | .key as $k | .value[]? | if $k != . then "\(.) -> \($k)" else "" end' \
-    | cat <(echo 'digraph G {') - <(echo '}')
+    | cat <(sed -n '/^ICON_ADVANCE_/s|^.*ICON_\(ADVANCE_[^[:space:]]*\).*FirstFrame[^"]*"CM2_\([^"]*\)".*$|\1 [image="\L\2"]|p' $1) - \
+    | cat <(echo 'digraph G {') \
+          <(echo 'imagepath="pic555"') \
+	  - \
+	  <(echo '}')
