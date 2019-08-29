96ce36360f55834f8704115563083cc9972b2b50
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2019-08-29 12:05:40 +0200

    use new images with CM2_, replacing _ by space in labels needs another new-line

--- tools/json2dot.sh
+++ tools/json2dot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 jq -r 'to_entries[] | .key as $k | .value[]? | if $k != . then "\(.) -> \($k)" else "" end' \
-    | cat <(sed -n '/^ICON_ADVANCE_/s|^.*ICON_ADVANCE_\([^[:space:]]*\).*FirstFrame[^"]*"\([^"]*\).TGA".*$|  ADVANCE_\1 [shape=box,\nlabel=<<TABLE border="0" cellspacing="0" cellborder="0"><TR><TD><IMG SCALE="TRUE" SRC="\L\2\E.png"/></TD></TR><TR><TD>\1</TD></TR></TABLE>>] |p' $1) - \
-    | sed '/label=/s/_/ /g' `# needs label= to be in a different line than node-name (\n above)!` \
+    | cat <(sed -n '/^ICON_ADVANCE_/s|^.*ICON_ADVANCE_\([^[:space:]]*\).*FirstFrame[^"]*"\([^"]*\).TGA".*$|  ADVANCE_\1 [shape=box,\nlabel=<<TABLE border="0" cellspacing="0" cellborder="0"><TR><TD><IMG SCALE="TRUE" SRC="\L\2\E.png"/></TD></TR>\n<TR><TD>\1</TD></TR></TABLE>>] |p' $1) - \
+    | sed '/^<TR><TD>/s/_/ /g' `# needs label= to be in a different line than node-name (\n above)!` \
     | cat <(echo 'digraph G {') \
           <(echo 'imagepath="pic555"') \
           <(echo 'rankdir=LR') \
