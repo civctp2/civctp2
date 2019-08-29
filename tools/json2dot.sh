9a1be899a6bddc2ec4af5fb5e53c865c966eb01e
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2019-08-29 10:31:47 +0200

    Revert "some image names are with CM2_ and some without", as expected since CityMod:
    https://github.com/civctp2/civctp2/pull/184#issuecomment-525908901
    
    This reverts commit f371d2649fe273b19bd7fbdce2e9f33455d9ec48.

--- tools/json2dot.sh
+++ tools/json2dot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 jq -r 'to_entries[] | .key as $k | .value[]? | if $k != . then "\(.) -> \($k)" else "" end' \
-    | cat <(sed -n '/^ICON_ADVANCE_/s|^.*ICON_ADVANCE_\([^[:space:]]*\).*FirstFrame[^"]*"[CM2_]*\([^"]*\).TGA".*$|  ADVANCE_\1 [shape=box,\nlabel=<<TABLE border="0" cellspacing="0" cellborder="0"><TR><TD><IMG SCALE="TRUE" SRC="\L\2\E.png"/></TD></TR><TR><TD>\1</TD></TR></TABLE>>] |p' $1) - \
+    | cat <(sed -n '/^ICON_ADVANCE_/s|^.*ICON_ADVANCE_\([^[:space:]]*\).*FirstFrame[^"]*"\([^"]*\).TGA".*$|  ADVANCE_\1 [shape=box,\nlabel=<<TABLE border="0" cellspacing="0" cellborder="0"><TR><TD><IMG SCALE="TRUE" SRC="\L\2\E.png"/></TD></TR><TR><TD>\1</TD></TR></TABLE>>] |p' $1) - \
     | sed '/label=/s/_/ /g' `# needs label= to be in a different line than node-name (\n above)!` \
     | cat <(echo 'digraph G {') \
           <(echo 'imagepath="pic555"') \
