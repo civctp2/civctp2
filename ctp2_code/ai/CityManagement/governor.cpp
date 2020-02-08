70af4ca4c7dcf7ecc913625944bcb88d2c31c838
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2020-02-08 15:53:41 +0100

    AI should not consider all possible new routes:
    because that causes the AI to waste caravans on low VPC routes when a new caravan becomes available
    instead it should just consider the first half of the sorted list (up to the median)
    if ther are not enough cravans available for any route up to the median, save the caravans for later turns
    
    instead of the median other thresholds could be used (e.g. 3rd quantile, mean, otsu, max-entropy etc)
    but are in general more difficult to realize for a list of floats (VPC) and not necessarily more appropriate
    
    some auto threshold implementations:
    https://github.com/fiji/Auto_Threshold/blob/cf263cafd29314ff96efb28062232c53d121c501/src/main/java/fiji/threshold/Auto_Threshold.java
    https://github.com/jpbalarini/otsus
    https://stackoverflow.com/questions/33693423/otsu-thresholding
    https://stackoverflow.com/questions/18219228/iterate-through-part-of-a-list-in-c#18219324

--- ctp2_code/ai/CityManagement/governor.cpp
+++ ctp2_code/ai/CityManagement/governor.cpp
@@ -5197,7 +5197,7 @@ void Governor::ManageGoodsTradeRoutes()
 	for
 	(
 		std::list<GoodsRoute>::iterator route_iter = new_routes.begin();
-		(route_iter != new_routes.end()) && (unused_freight > 0); // if unused caravans are still available
+		(route_iter != std::next(new_routes.begin(), new_routes.size() / 2)) && (unused_freight > 0); // only iterate over first half (i.e. use median as threshold) to not waste cravans on low VPC instead save them for next turn && if unused caravans are still available
 		++route_iter
 	)
 	{
