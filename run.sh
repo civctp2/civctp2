22778e0d3f49a85f0d7b56f63944dbacf2751f0a
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2020-03-24 17:05:06 +0100

    mount Scenarios/ folder into DI, which enables edits and saving of new scenarios

--- run.sh
+++ run.sh
@@ -14,6 +14,7 @@ fi
 
 mkdir -p $HOME/.civctp2/save/
 mkdir -p $HOME/.civctp2/logs/ # if not existent created by docker then owned by root
+mkdir -p $HOME/.civctp2/Scenarios/ # if not existent created by docker then owned by root
 touch $HOME/.civctp2/userprofile.txt # file must exist for docker file-vol
 touch $HOME/.civctp2/userkeymap.txt # file must exist for docker file-vol
 
@@ -32,6 +33,7 @@ docker run \
        $CON \
        -v $HOME/.civctp2/save/:/opt/ctp2/ctp2_program/ctp/save \
        -v $HOME/.civctp2/logs/:/opt/ctp2/ctp2_program/ctp/logs \
+       -v $HOME/.civctp2/Scenarios/:/opt/ctp2/Scenarios \
        $@
 
 if [ -z ${XVFBtmp+x} ]; then 
