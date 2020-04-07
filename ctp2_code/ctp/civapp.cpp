8f07684335006044b72888e1f9ae11eb93fc90c5
William Deurwaarder <william.deurwaarder@tomtom.com>
2020-04-08 00:17:32 +0200

    Use bionic (18.04) on Travis to remove the _IO_cookie_io_functions_t error
    Update gtk 2.0 library reference.

--- ctp2_code/ctp/civapp.cpp
+++ ctp2_code/ctp/civapp.cpp
@@ -39,9 +39,9 @@
 // - Keep the user's leader name when the data is consistent.
 // - Skip begin turn handling when loading from a file.
 // - Fixed a repetitive memory leak in the Great Libary caused by
-//   scenario loading, by Martin G?hmann.
+//   scenario loading, by Martin Gühmann.
 // - Removed some redundant code, because it is already done
-//   somewhere else, by Martin G?hmann.
+//   somewhere else, by Martin Gühmann.
 //
 //----------------------------------------------------------------------------
 //
@@ -65,32 +65,32 @@
 // - Used the new ColorSet option to select civilisation colors.
 // - Memory leak repaired: clean up the turn counter override information.
 // - Hot seat handling improved.
-// - Static member of StatusBar is now deleted correctly, by Martin G?hmann.
+// - Static member of StatusBar is now deleted correctly, by Martin Gühmann.
 // - Cleaned up music screen.
 // - The civilisation index from the profile is now reset if it is too high.
-//   This prevents the game from crashing. - April 12th 2005 Martin G?hmann
+//   This prevents the game from crashing. - April 12th 2005 Martin Gühmann
 // - Added crash prevention during game loading.
 // - Added another civilisation index check.
 // - Option added to include multiple data directories.
 // - Added Slic segment cleanup.
-// - Replaced old civilisation database by new one. (Aug 22nd 2005 Martin G?hmann)
-// - Made progress bar more fluently. (Aug 22nd 2005 Martin G?hmann)
-// - Removed the old endgame and installation databases. (Aug 29th 2005 Martin G?hmann)
+// - Replaced old civilisation database by new one. (Aug 22nd 2005 Martin Gühmann)
+// - Made progress bar more fluently. (Aug 22nd 2005 Martin Gühmann)
+// - Removed the old endgame and installation databases. (Aug 29th 2005 Martin Gühmann)
 // - Removed old sprite state databases, removed olf good's icon
-//   database (unused), replaced old risk database by new one. (Aug 29th 2005 Martin G?hmann)
-// - The right color set is now selected afterwards the ProfileDB is available. (Aug 29th 2005 Martin G?hmann)
-// - Added cleanup of gaia controller and info window. (Sep 13th 2005 Martin G?hmann)
-// - Added ArmyData and Network cleanup. (Sep 25th 2005 Martin G?hmann)
-// - Added graphicsresscreen_Cleanup. (Sep 25th 2005 Martin G?hmann)
-// - Replaced old difficulty database by new one. (April 29th 2006 Martin G?hmann)
-// - Replaced old pollution database by new one. (July 15th 2006 Martin G?hmann)
-// - Replaced old global warming database by new one. (July 15th 2006 Martin G?hmann)
-// - Added new map icon database. (3-Mar-2007 Martin G?hmann)
-// - Replaced old map database by new one. (27-Mar-2007 Martin G?hmann)
-// - Replaced old concept database by new one. (31-Mar-2007 Martin G?hmann)
-// - Replaced old const database by new one. (5-Aug-2007 Martin G?hmann)
-// - Fixed PBEM BeginTurn event execution. (27-Oct-2007 Martin G?hmann)
-// - Games can now be saved if the visible player is a robot. (30-Jan-2008 Martin G?hmann)
+//   database (unused), replaced old risk database by new one. (Aug 29th 2005 Martin Gühmann)
+// - The right color set is now selected afterwards the ProfileDB is available. (Aug 29th 2005 Martin Gühmann)
+// - Added cleanup of gaia controller and info window. (Sep 13th 2005 Martin Gühmann)
+// - Added ArmyData and Network cleanup. (Sep 25th 2005 Martin Gühmann)
+// - Added graphicsresscreen_Cleanup. (Sep 25th 2005 Martin Gühmann)
+// - Replaced old difficulty database by new one. (April 29th 2006 Martin Gühmann)
+// - Replaced old pollution database by new one. (July 15th 2006 Martin Gühmann)
+// - Replaced old global warming database by new one. (July 15th 2006 Martin Gühmann)
+// - Added new map icon database. (3-Mar-2007 Martin Gühmann)
+// - Replaced old map database by new one. (27-Mar-2007 Martin Gühmann)
+// - Replaced old concept database by new one. (31-Mar-2007 Martin Gühmann)
+// - Replaced old const database by new one. (5-Aug-2007 Martin Gühmann)
+// - Fixed PBEM BeginTurn event execution. (27-Oct-2007 Martin Gühmann)
+// - Games can now be saved if the visible player is a robot. (30-Jan-2008 Martin Gühmann)
 //
 //----------------------------------------------------------------------------
 
