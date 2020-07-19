c2c445925796f35b59dd7c0f9f58d8c7608c52b2
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2020-07-19 15:52:58 +0200

    xlabel to list units enabled by advance

--- tools/json2dot.sh
+++ tools/json2dot.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 jq -r 'to_entries[] | .key as $k | .value[]? | if $k != . then if . | startswith("AGE") then "\($k) [group=\(.)]" else "\(.) -> \($k)" end else "" end' \
-    | cat <(sed -n '/^ICON_ADVANCE_/s|^.*ICON_ADVANCE_\([^[:space:]]*\).*FirstFrame[^"]*"\([^"]*\).TGA".*$|  ADVANCE_\1 [shape=box,\nlabel=<<TABLE border="0" cellspacing="0" cellborder="0"><TR><TD><IMG SCALE="TRUE" SRC="\L\2\E.png"/></TD></TR>\n<TR><TD><B>\1</B></TD></TR></TABLE>>] |p' $1) - \
+    | cat <(sed -n '/^ICON_ADVANCE_/s|^.*ICON_ADVANCE_\([^[:space:]]*\).*FirstFrame[^"]*"\([^"]*\).TGA".*$|  ADVANCE_\1 [shape=box,\nlabel=<<TABLE border="0" cellspacing="0" cellborder="0"><TR><TD><IMG SCALE="TRUE" SRC="\L\2\E.png"/></TD></TR>\n<TR><TD><B>\1</B></TD></TR></TABLE>>] |p' ../ctp2_data/default/gamedata/uniticon.txt ) \
+          <(awk '/{/ {a=$1}; $1 ~ /^EnableAdvance$/ {{printf("%s [xlabel=%s]\n", $2, a)}}' ../ctp2_data/default/gamedata/Units.txt ) \
+          - \
     | sed '/^<TR><TD>/s/_/ /g' `# needs label= to be in a different line than node-name (\n above)!` \
     | cat <(echo 'digraph G {') \
           <(echo 'imagepath="pic555"') \
