baea12753252446d8eb353a7b273da0654993e1a
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2020-08-18 01:47:54 +0200

    left-align cell text:
    https://renenyffenegger.ch/notes/tools/Graphviz/attributes/label/HTML-like/index

--- tools/json2dot.sh
+++ tools/json2dot.sh
@@ -2,7 +2,7 @@
 
 jq -r 'to_entries[] | .key as $k | .value[]? | if $k != . then if . | startswith("AGE") then "\($k) [group=\(.)]" else "\(.) -> \($k)" end else "" end' \
     | sed 's/ADVANCE_/node_/g' \
-    | cat <(sed -n '/^ICON_ADVANCE_/s|^.*ICON_ADVANCE_\([^[:space:]]*\).*FirstFrame[^"]*"\([^"]*\).TGA".*$|  node_\1 [shape=box, label=<<TABLE border="0" cellspacing="0" cellborder="0"><TR><TD><IMG SCALE="TRUE" SRC="\L\2\E.png"/></TD></TR><TR><TD><B>ADVANCE_\1</B></TD></TR></TABLE>>] |p' ../ctp2_data/default/gamedata/uniticon.txt ) \
+    | cat <(sed -n '/^ICON_ADVANCE_/s|^.*ICON_ADVANCE_\([^[:space:]]*\).*FirstFrame[^"]*"\([^"]*\).TGA".*$|  node_\1 [shape=box, label=<<TABLE border="0" cellspacing="0" cellborder="0"><TR><TH><IMG SCALE="TRUE" SRC="\L\2\E.png"/></TH></TR><TR><TH><B>ADVANCE_\1</B></TH></TR></TABLE>>] |p' ../ctp2_data/default/gamedata/uniticon.txt ) \
           - \
     | cat <(echo 'digraph G {') \
           <(echo 'imagepath="pic555"') \
