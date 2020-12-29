383e63cce9b8787d10e845e87d759bc9673c6446
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2020-12-29 23:10:24 +0100

    dpkg-deb expects dirs with 755 to avoid:
    error: control directory has bad permissions 777 (must be >=0755 and <=0775)
    
    https://tldp.org/HOWTO/html_single/Debian-Binary-Package-Building-HOWTO/#AEN92

--- tools/build-deb.sh
+++ tools/build-deb.sh
@@ -10,5 +10,6 @@ BTYP=$1
 mkdir -p /debian/opt/ctp2/
 cp -r /opt/ctp2/ /debian/opt/
 
+find ./debian -type d | xargs chmod 755 # to avoid error: control directory has bad permissions 777 (must be >=0755 and <=0775)
 dpkg-deb --build /debian # uses debian/ to built debian.deb
 mv /debian.deb /deb/ctp2-$BTYP.deb
