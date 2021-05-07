245f5e125b1b0ab6ce2e181c6176f2fa5fe78c8c
William Deurwaarder <william.git@xs4all.nl>
2021-05-07 20:40:08 +0200

    Animate goods in help-tile window

--- ctp2_code/ui/aui_ctp2/tilecontrol.h
+++ ctp2_code/ui/aui_ctp2/tilecontrol.h
@@ -4,44 +4,31 @@
 #ifndef __TILECONTROL_H__
 #define __TILECONTROL_H__
 
-
 class aui_Surface;
 class MapPoint;
-
+class GoodActor;
 
 class TileControl : public aui_Control
 {
 public:
+	TileControl(AUI_ERRCODE * retval, uint32 id, MBCHAR * ldlBlock);
+	TileControl(AUI_ERRCODE * retval, uint32 id, sint32 x, sint32 y, sint32 width, sint32 height,
+			 ControlActionCallback * ActionFunc = NULL, void * cookie = NULL);
+	virtual ~TileControl();
 
-	TileControl(AUI_ERRCODE *retval,
-						uint32 id,
-						MBCHAR *ldlBlock );
-	TileControl(
-		AUI_ERRCODE *retval,
-		uint32 id,
-		sint32 x,
-		sint32 y,
-		sint32 width,
-		sint32 height,
-		ControlActionCallback *ActionFunc = NULL,
-		void *cookie = NULL );
-	virtual ~TileControl() {};
+	virtual AUI_ERRCODE DrawThis(aui_Surface * surface = NULL, sint32 x = 0, sint32 y = 0);
 
-	virtual AUI_ERRCODE DrawThis(
-		aui_Surface *surface = NULL,
-		sint32 x = 0,
-		sint32 y = 0 );
-
-	void SetMouseTile(const MapPoint &p) { m_currentTile = p; };
+	void SetMouseTile(const MapPoint & pos);
 
 protected:
-	MapPoint			m_currentTile;
+	virtual void PostChildrenCallback(aui_MouseEvent * mouseData);
 
-	sint32 DrawTile(aui_Surface *surface,
-			sint32 i,
-			sint32 j,
-			sint32 x,
-			sint32 y);
+private:
+	bool DrawTile(aui_Surface * surface, sint32 x, sint32 y);
+
+	MapPoint    m_currentTile;
+	GoodActor * m_goodActor;
+	uint32      m_nextProcessTime;
 };
 
 #endif
