6558df12b80e9feb1842aa37b85b19d7da812dff
Matin Gühmann <guehmann@t-online.de>
2025-05-17 01:45:29 +0200

    Make the BioInfectComple message show again. Even so you have the special effect, you still expect a message
    Beside that you can delete the message with a right click, very quickly

--- ctp2_code/gs/gameobj/unitevent.cpp
+++ ctp2_code/gs/gameobj/unitevent.cpp
@@ -386,13 +386,11 @@ STDEHANDLER(BioInfectCityUnitEvent)
 	so->AddCity(c) ;
 	g_slicEngine->Execute(so) ;
 
-	/* disabled because success of action is obvious any way (special effect and city icon) and therefore message spamming can be avoided
 	so = new SlicObject("11iBioInfectComplete") ;
 	so->AddRecipient(u.GetOwner()) ;
 	so->AddCivilisation(c.GetOwner()) ;
 	so->AddCity(c) ;
 	g_slicEngine->Execute(so) ;
-	*/
 
 	g_gevManager->AddEvent(GEV_INSERT_AfterCurrent, GEV_BioInfectCity,
 						   GEA_City, c.m_id,
